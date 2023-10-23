dff908336781057ca0731753c599d97029c10954
Ankit Malaviya <ankitmalaviya13@yahoo.com>
2023-10-23 14:40:45 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const db = require("./config/db.config.js");
+// const db = require("./config/db.config.js");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
