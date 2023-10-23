96dcc93260f8ed4ee23a0d20f5b0bc112ca8b787
Ankit Malaviya <ankitmalaviya13@yahoo.com>
2023-10-23 14:44:07 +0530

    first commit

--- config/db.config.js
+++ config/db.config.js
@@ -1,17 +1,17 @@
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
-const db = mysql.createConnection({
-  host: "databases.000webhost.com",
-  user: "id21441588_reesy",
-  password: "Cubes@4030",
-  database: "id21441588_reesy",
-  charset: 'utf8mb4',
-  // socketPath: '/var/run/mysqld/mysqld.sock',
-});
+// const db = mysql.createConnection({
+//   host: "databases.000webhost.com",
+//   user: "id21441588_reesy",
+//   password: "Cubes@4030",
+//   database: "id21441588_reesy",
+//   charset: 'utf8mb4',
+//   // socketPath: '/var/run/mysqld/mysqld.sock',
+// });
 
-db.connect(function (err) {
-  if (err) throw err;
-  console.log(" DB Connected!");
-});
+// db.connect(function (err) {
+//   if (err) throw err;
+//   console.log(" DB Connected!");
+// });
 
-module.exports = db;
+// module.exports = db;
